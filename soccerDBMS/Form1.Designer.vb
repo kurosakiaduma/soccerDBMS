98d766da33b79c5c35878ee8278a7778f4921970
Tevin Joel <tevin74@live.com>
2018-09-20 08:28:15 +0300

    exit thread working succs

--- soccerDBMS/Form1.Designer.vb
+++ soccerDBMS/Form1.Designer.vb
@@ -27,11 +27,13 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(177, 170)
+        Me.Button1.Location = New System.Drawing.Point(27, 168)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(94, 32)
         Me.Button1.TabIndex = 0
@@ -72,11 +74,31 @@ Partial Class Form1
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Password"
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(27, 217)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(243, 32)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Forgot password?"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(177, 168)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(93, 32)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
@@ -94,4 +116,6 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- soccerDBMS/Form1.vb
+++ soccerDBMS/Form1.vb
@@ -24,4 +24,8 @@
             MsgBox("Login failed. Try again.", MsgBoxStyle.Critical, "KPL")
         End If
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Application.Exit()
+    End Sub
 End Class
